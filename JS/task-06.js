b3912a0547010eae7277e4e922b476d87f4af163
Grigoriy Chernish <cga.jsdev@gmail.com>
2020-10-25 00:34:43 +0300

    Исправил ошибку

--- JS/task-06.js
+++ JS/task-06.js
@@ -9,7 +9,7 @@ const inputRef = document.querySelector('#validation-input');
 inputRef.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-  if (event.currentTarget.value.length >= inputRef.dataset.length) {
+  if (event.currentTarget.value.length === inputRef.dataset.length) {
     inputRef.classList.add('valid');
     inputRef.classList.remove('invalid');
   } else {
